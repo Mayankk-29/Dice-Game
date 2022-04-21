a133435523ad9718f914dafa01535839c02d5496
Mayankk-29 <85258390+Mayankk-29@users.noreply.github.com>
2022-04-21 05:35:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var rn1 = Math.floor((Math.random()*6) + 1);
+var s1 = "images/" + rn1 + ".png";
+var dice1 = document.querySelectorAll("img")[0];
+dice1.setAttribute("src",s1);
+
+var rn2 = Math.floor((Math.random()*6) + 1);
+var s2 = "images/" + rn2 + ".png";
+var dice2 = document.querySelectorAll("img")[1];
+dice2.setAttribute("src",s2);
+
+if(rn1>rn2) document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+else if(rn1<rn2) document.querySelector("h1").innerHTML = "🚩 Play 2 Wins!";
+else document.querySelector("h1").innerHTML = "Draw!";
